d19a3bbdf7fb22c0695fc11da4db4e7000375472
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-04-16 09:47:01 +0200

    [UPD] argv

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function hello(message) {
 
 hello("Hello, Boolean!");
 hello("Hip HIp Hurra!"); //bonus
+
+const a = process.argv[2] ?? 'world'; //?? è  un valore che viene usato se il primo è undefined
+
+console.log(`Hello, ${a}!`)
